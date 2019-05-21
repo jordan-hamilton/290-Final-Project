b81471fc10557d6f7736cfa08eddd3d2940528fb
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-05-21 16:18:26 -0600

    Add carousel

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,8 @@
 import navBar from './navBar.js';
+import carousel from './carousel.js';
+
 var nav = navBar();
+var intro = carousel();
 
 document.addEventListener('DOMContentLoaded', function(event) {
 
