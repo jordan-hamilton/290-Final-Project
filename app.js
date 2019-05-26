db63a2516361a1e26b9196de6e84b6c86cb40da4
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-05-26 08:36:52 -0600

    Add placeholder for Handlebars

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
-var path = require('path');
 var bodyParser = require('body-parser');
+var handlebars = require('express-handlebars').create({
+  defaultLayout: 'main'
+});
+var path = require('path');
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
-
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 // Configure body-parser
@@ -28,6 +32,17 @@ app.get('/list', function(req, res) {
 app.get('/add', function(req, res) {
   res.sendFile(path.join(__dirname, 'pages', 'add.html'));
 });
+
+/*app.use(function(req, res) {
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});*/
 
 app.listen(app.get('port'), function() {
   console.log('Server started on http://localhost:' + app.get('port') + '.');
