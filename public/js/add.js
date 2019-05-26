3cb4198eec858ecceeea978589d95ae7b0bc92f5
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-05-26 16:07:56 -0600

    Add default layout

--- public/js/add.js
+++ public/js/add.js
@@ -1,6 +1,12 @@
 import navBar from './navBar.js';
 var nav = navBar();
 
+/*<form action="/" method="post">
+  <label for="name">Your Name:</label>
+  <input id="name" type="text" name="name">
+  <input type="submit" name="New List">
+</form>*/
+
 document.addEventListener('DOMContentLoaded', function(event) {
 
 });
