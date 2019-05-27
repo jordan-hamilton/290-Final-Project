ba1d3629c70f1d051181820c464b4cfba82be4b6
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-05-26 18:29:09 -0600

    Working API requests

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var bodyParser = require('body-parser');
 var express = require('express');
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var handlebars = require('express-handlebars').create({
+  defaultLayout: 'main'
+});
 var path = require('path');
+var request = require('request');
 
 var api = require('./api/queries.js');
 
-//Configure Express
+// Configure Express
 var app = express();
 app.set('port', process.env.PORT || 3000);
-// Set the public as a static directory
+// Set the public folder as a static directory
 app.use(express.static('public'));
 
 // Configure body-parser
@@ -17,7 +20,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-//Configure Handlebars
+// Configure Handlebars
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -38,6 +41,22 @@ app.get('/add', function(req, res) {
   res.sendFile(path.join(__dirname, 'pages', 'add.html'));
 });
 
+app.get('/test', function(req, res) {
+  request('http://localhost:3000/api/buildings', function(error, response, body) {
+    if (!error && response.statusCode < 400) {
+      console.log(body);//DEBUG
+      console.log(response.statusCode);//DEBUG
+      console.log(response);//DEBUG
+      res.render('test', parseJson(body));
+    } else {
+      if (response) {
+        console.log(response.statusCode);
+      }
+      next(err);
+    }
+  });
+});
+
 //Set GET requests for API queries
 app.get('/api/buildings', api.getBuildings);
 app.get('/api/buildings/:id', api.getBuildingById);
@@ -49,3 +68,16 @@ app.get('/api/technicians/:id', api.getTechnicianById);
 app.listen(app.get('port'), function() {
   console.log('Server started on http://localhost:' + app.get('port') + '.');
 });
+
+function parseJson(body) {
+  var context = {};
+  context.data = [];
+  var content = JSON.parse(body)
+  for (var item in content) {
+    context.data.push({
+      'name': content[item].name
+      'value': content[item].id
+    });
+  }
+  return context;
+}
