83004c39dd4e346038ce89f709ba1a3d71f64cca
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-05-28 21:22:59 -0600

    Remove unused event listener

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,7 @@ import Carousel from './carousel.js';
 var nav = NavBar;
 var carousel = Carousel;
 
-// Import the navigation bar
+// Display the navigation bar
 document.getElementById('app').insertAdjacentElement('beforebegin', nav());
 // Add the carousel to the homepage
 document.getElementById('app').appendChild(carousel());
-
-document.addEventListener('DOMContentLoaded', function(event) {
-
-});
