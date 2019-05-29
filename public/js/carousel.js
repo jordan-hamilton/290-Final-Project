5f754cbf0c26b1364d49718099f2e0d254cde0f2
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-05-28 21:22:28 -0600

    Remove caption placeholder

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -49,15 +49,6 @@ function createCarousel() {
 
   carousel.appendChild(carouselIndicators);
 
-
-  /*<div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </div>
-    </div>*/
   var innerCarousel = document.createElement('div');
   innerCarousel.setAttribute('class', 'carousel-inner');
 
